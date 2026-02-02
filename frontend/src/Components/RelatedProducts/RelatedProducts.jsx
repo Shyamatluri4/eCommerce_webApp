84e71e989a2eaae040e773285115b056eb2682a3
Shyamatluri4 <avmshyamreddy123@gmail.com>
2026-02-02 21:26:46 +0530

    resolved the frontend issues

--- frontend/src/Components/RelatedProducts/RelatedProducts.jsx
+++ frontend/src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,32 +1,40 @@
-import React, { useEffect, useState } from 'react'
-import './RelatedProducts.css'
-import Item from '../Item/Item'
+import React, { useEffect, useState } from "react";
+import "./RelatedProducts.css";
+import Item from "../Item/Item";
 
 const RelatedProducts = (props) => {
-
   const [relatedProducts, setRelatedProducts] = useState([]);
-  const {product} = props;
+  const { product } = props;
 
   useEffect(() => {
-    console.log(product)
+    console.log(product);
     // var category = props.product.category;
     // console.log(category.toLowerCase());
-    fetch(`http://localhost:4000/related-${product.category}`)
-    .then(res => res.json())
-    .then(data => setRelatedProducts(data));
-  },[])
+    fetch(`http://localhost:5000/related-${product.category}`)
+      .then((res) => res.json())
+      .then((data) => setRelatedProducts(data));
+  }, [product]);
 
   return (
-    <div className='relatedproducts'>
+    <div className="relatedproducts">
       <h1>Related Products</h1>
       <hr />
       <div className="relatedproducts-item">
-        {relatedProducts.map((item,i) => {
-            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+        {relatedProducts.map((item, i) => {
+          return (
+            <Item
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            />
+          );
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default RelatedProducts
+export default RelatedProducts;
